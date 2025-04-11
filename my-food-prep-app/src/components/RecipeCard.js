045cfa930a41e5f0dff7d47b30e731b58c6f1f44
Mary <marynevermann@gmail.com>
2025-04-11 22:28:44 +0200

    added contain for recipe card file

--- my-food-prep-app/src/components/RecipeCard.js
+++ my-food-prep-app/src/components/RecipeCard.js
@@ -1,1 +1,12 @@
 //card for displaying recipes
+import {Link} from 'react-router-dom';
+
+export default function RecipeCard({recipe}){
+    return(
+        <div className='recipe-card'>
+        <h2>{recipe.title}</h2>
+        <p>Time: {recipe.duration}</p>
+        <Link to={`/recipe/${recipe.id}`}> View Recipe</Link>
+        </div>
+    );
+}
