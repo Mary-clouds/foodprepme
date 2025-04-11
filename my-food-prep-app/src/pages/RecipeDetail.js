a8419e2cdeab49cfcb45dffcf655ae1e7223c50d
Mary <marynevermann@gmail.com>
2025-04-11 22:41:14 +0200

    added the map for recipedetail file

--- my-food-prep-app/src/pages/RecipeDetail.js
+++ my-food-prep-app/src/pages/RecipeDetail.js
@@ -1,1 +1,22 @@
 //single recipe view
+import React from 'react';
+import {useParams} from 'react-router-dom';
+
+export default function RecipeDetail(){
+    const {id} = useParams();
+    const recipe = {id, title: 'Lentil curry', ingredients: ['Lentils', 'Coconut Milk', 'Spices'], step: ['Cook Lentils', 'Add Coconut Milk', 'Simmer with Spices']};
+
+    return(
+        <div>
+            <h1>{recipe.title}</h1>
+            <h2>Ingredients:</h2>
+            <ul>
+                {recipe.ingredients.map((ingredients, index)=>(
+                    <li key={index}>{ingredients}</li>
+                ))}
+            </ul>
+            <ol>{recipe.steps.map((step, index)=>(<li key={index}>{step}</li>))}</ol>
+        </div>
+    );
+
+}
