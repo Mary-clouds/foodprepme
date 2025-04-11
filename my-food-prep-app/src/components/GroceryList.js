46499a3f4af9c417e727e3cb0e9892aafd0bdb21
Mary <marynevermann@gmail.com>
2025-04-11 22:45:57 +0200

    added contain for the grocerylist file

--- my-food-prep-app/src/components/GroceryList.js
+++ my-food-prep-app/src/components/GroceryList.js
@@ -1,1 +1,17 @@
 //to display the grocery list
+export default function GroceryList(){
+    const groceryItems =['Lentils', 'Coconut Milk', 'Spices'];
+
+    return(
+        <div>
+            <h1>
+                Grocery List
+            </h1>
+            <ul>
+                {groceryItems.map((item, index)=>(
+                    <li key={index}>{item}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
