49c8807660db1e52c67449731e47a6671b1b8cb2
Mary <marynevermann@gmail.com>
2025-04-11 22:06:52 +0200

    added a header with navigation bar

--- my-food-prep-app/src/App.jsx
+++ my-food-prep-app/src/App.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './pages/Home';
 import RecipeDetail from './pages/RecipeDetail';
-
+import Header from './components/Header';
 
 function App() {
   return (
     <Router>
+      <Header/>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/recipe/:id' element={<RecipeDetail/>}/>
--- my-food-prep-app/src/components/Header.js
+++ my-food-prep-app/src/components/Header.js
@@ -1,1 +1,21 @@
 //navigation bar
+import {Link} from 'react-router-dom';
+
+function Header(){
+    return(
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/GroceryList">Grocery List</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
