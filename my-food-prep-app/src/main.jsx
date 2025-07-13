af8e3ebff9e9277f081b3987bef1c7d27b8290ed
Mary <marynevermann@gmail.com>
2025-07-13 13:27:05 +0200

    updated main.jsx

--- my-food-prep-app/src/main.jsx
+++ my-food-prep-app/src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { GroceryProvider } from './GroceryContext';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <GroceryProvider>
+      <App />
+    </GroceryProvider>
+    
   </StrictMode>,
 )
